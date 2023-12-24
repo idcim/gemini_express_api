37af53de12592629f768772b9f8b18eb9bc2bace
idcim@qq.com <idcim@qq.com>
2023-12-25 01:43:34 +0800

    test docker

--- plugins/gemini/index.js
+++ plugins/gemini/index.js
@@ -47,22 +47,22 @@ async function text(prompt,history=[]) {
   };
   
   const safetySettings = [
-    // {
-    //   category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-    //   threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    // },
-    // {
-    //   category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-    //   threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    // },
-    // {
-    //   category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-    //   threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    // },
-    // {
-    //   category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-    //   threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    // },
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
   ];
   
   const chats = model.startChat({
